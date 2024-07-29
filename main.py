f4348b41f0ac92085f88805acc8564a80f97db94
gaboree <gaborzsolt2007@yahoo.com>
2024-07-29 14:32:06 +0300

    OOP 4.9

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-class Hero:
-    def __init__(self, name, health):
+class Human:
+    def __init__(self, name):
         self.__name = name
-        self.__health = health
 
     def get_name(self):
         return self.__name
 
+
+## don't touch above this line
+
+
+class Archer(Human):
+    def __init__(self, name, health, num_arrows):
+        super().__init__(name)
+        self.__num_arrows = num_arrows
+        self.__health = health
+
+    def get_num_arrows(self):
+        return self.__num_arrows
+    
+    def get_name(self):
+        return super().get_name()
+    
     def get_health(self):
         return self.__health
-
-    def take_damage(self, damage):
-        self.__health -= damage
-
-
-class Archer(Hero):
-    def __init__(self, name, health, num_arrows):
-        super().__init__(name, health)
-        self.__num_arrows = num_arrows
 
     def shoot(self, target):
         if self.__num_arrows > 0:
@@ -24,3 +30,44 @@ class Archer(Hero):
             target.take_damage(10)
         else:
             raise Exception("not enough arrows")
+    
+    def take_damage(self, damage):
+        self.__health -= damage
+
+
+
+class Crossbowman(Archer):
+    def __init__(self, name, num_arrows):
+        super().__init__(name, num_arrows)
+
+    def triple_shot(self, target):
+        if self.get_num_arrows() >= 3:
+            self.use_arrows(3)
+            return f"{target.get_name()} was shot by 3 crossbow bolts"
+        else:
+            raise Exception("not enough arrows")
+
+class Wizard(Human):
+    def __init__(self, name, health, mana):
+        super().__init__(name)
+        self.__health = health
+        self.__mana = mana
+    
+    def get_name(self):
+        return super().get_name()
+    
+    def get_health(self):
+        return self.__health
+
+    def get_mana(self):
+        return self.__mana
+    
+    def take_damage(self, damage):
+        self.__health -= damage
+
+    def cast(self, target):
+        if self.get_mana() >= 25:
+            target.take_damage(25)
+            self.__mana -= 25
+        else:
+            raise Exception("not enough mana")
--- main_test.py
+++ main_test.py
@@ -1,29 +1,67 @@
 from main import *
 
 run_cases = [
-    (Hero("Hercules", 200), Archer("Pericles", 100, 2), 190),
-    (Hero("Aquiles", 150), Archer("Aneas", 80, 1), 140),
+    (
+        Wizard("Harry", 30, 70),
+        Archer("Pericles", 100, 3),
+        ["cast", "shoot", "shoot"],
+        [10, 75],
+    ),
+    (
+        Wizard("Ron", 50, 90),
+        Archer("Odysseus", 80, 2),
+        ["shoot", "shoot", "shoot", "cast"],
+        [None, None],
+        "not enough arrows",
+    ),
 ]
 
 submit_cases = run_cases + [
-    (Hero("Paris", 120), Archer("Hector", 100, 0), None, "not enough arrows"),
-    (Hero("Helen", 100), Archer("Menelaus", 50, 2), 90),
-    (Hero("Odysseus", 90), Archer("Circe", 70, 1), 80),
-    (Hero("Icarus", 60), Archer("Daedalus", 40, 2), 40, None, True),
-    (Hero("Zeus", 1000), Archer("Hades", 900, 1), None, "not enough arrows", True),
+    (Wizard("Hermoine", 45, 25), Archer("Achilles", 60, 1), ["cast"], [45, 35]),
+    (
+        Wizard("Dumbledore", 100, 150),
+        Archer("Hercules", 120, 4),
+        ["shoot"],
+        [90, 120],
+    ),
+    (
+        Wizard("Snape", 80, 24),
+        Archer("Theseus", 70, 3),
+        ["cast"],
+        [None, None],
+        "not enough mana",
+    ),
+    (
+        Wizard("Luna", 65, 49),
+        Archer("Paris", 85, 2),
+        ["cast", "shoot", "shoot", "cast"],
+        [None, None],
+        "not enough mana",
+    ),
+    (
+        Wizard("Neville", 55, 45),
+        Archer("Hector", 75, 3),
+        ["shoot", "cast"],
+        [45, 50],
+    ),
 ]
 
 
-def test(hero, archer, expected_result, expected_err=None, twice=False):
+def test(wizard, archer, actions, expected_result, expected_err=None):
     print("---------------------------------")
-    print(f"Hero: {hero.get_name()} with health: {hero.get_health()}")
-    print(f"Archer: {archer.get_name()} with health: {archer.get_health()}")
+    print(f"Inputs:")
+    print(f" * Wizard: {wizard.get_name()}, HP: {wizard.get_health()}")
+    print(f" * Archer: {archer.get_name()}, HP: {archer.get_health()}")
+    print(f"Actions: {actions}")
+
     try:
-        archer.shoot(hero)
-        if twice:
-            print("Shooting twice!")
-            archer.shoot(hero)
-        result = hero.get_health()
+        for action in actions:
+            if action == "cast":
+                print(f"{wizard.get_name()} casts a spell at {archer.get_name()}")
+                wizard.cast(archer)
+            elif action == "shoot":
+                print(f"{archer.get_name()} shoots an arrow at {wizard.get_name()}")
+                archer.shoot(wizard)
 
         if expected_err:
             print(f"Expected Exception: {expected_err}")
@@ -31,16 +69,25 @@ def test(hero, archer, expected_result, expected_err=None, twice=False):
             print("Fail")
             return False
 
-        print(f"Expecting Hero's health after shoot: {expected_result}")
-        print(f"Actual: {result}")
-        if result == expected_result:
+        wizard_hp = wizard.get_health()
+        archer_hp = archer.get_health()
+        print(
+            f"Expected: {wizard.get_name()} HP: {expected_result[0]}, {archer.get_name()} HP: {expected_result[1]}"
+        )
+        print(
+            f"Actual: {wizard.get_name()} HP: {wizard_hp}, {archer.get_name()} HP: {archer_hp}"
+        )
+
+        if wizard_hp == expected_result[0] and archer_hp == expected_result[1]:
             print("Pass")
             return True
-        print("Fail")
-        return False
+        else:
+            print("Fail")
+            return False
+
     except Exception as e:
         print(f"Expected Exception: {expected_err}")
-        print(f"Actual Exception: {e}")
+        print(f"Actual Exception: {str(e)}")
         if str(e) == expected_err:
             print("Pass")
             return True
