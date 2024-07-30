68e5511a90d87b1944a417c5dcb5df21815daca5
gaboree <gaborzsolt2007@yahoo.com>
2024-07-30 12:44:28 +0300

    OOP 5.4

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x1, y1, x2, y2):
+        pass
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, height, width, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.fire_range = fire_range
+        self.height = height
+        self.width = width
+        half_width = width/2
+        half_height = height/2
+        self.__hit_box__ = Rectangle(pos_x - half_width, pos_y - half_height,
+                                     pos_x + half_width, pos_y + half_height)
+        
+
+    def in_area(self, x1, y1, x2, y2):
+        area_rect = Rectangle(x1, y1, x2, y2)
+        return self.__hit_box__.overlaps(area_rect)
+
+# Don't touch below this line
+
+
 class Rectangle:
     def overlaps(self, rect):
-        return (self.get_left_x() <= rect.get_right_x() and
-                self.get_right_x() >= rect.get_left_x() and
-                self.get_top_y() >= rect.get_bottom_y() and
-                self.get_bottom_y() <= rect.get_top_y())
-
-    # don't touch below this line
+        return (
+            self.get_left_x() <= rect.get_right_x()
+            and self.get_right_x() >= rect.get_left_x()
+            and self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
+        )
 
     def __init__(self, x1, y1, x2, y2):
         self.__x1 = x1
@@ -32,6 +61,3 @@ class Rectangle:
         if self.__y1 < self.__y2:
             return self.__y1
         return self.__y2
-
-    def __repr__(self):
-        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
